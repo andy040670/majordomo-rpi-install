a0d45831ac74ae9f5b4e2bdb8c5870cff2d6c066
andy040670 <andy040670@yandex.ru>
2023-06-19 17:34:10 +0300

    Update install_redis.sh
    Change home directory ntml to www

--- sections/install_redis.sh
+++ sections/install_redis.sh
@@ -3,8 +3,8 @@
 showMessage "Installing Redis for caching..."
 
 runSudo "apt-get install -y redis"
-#replaceString "/var/www/html/config.php" "\/\/define('USE_REDIS" "\/\/\ndefine('USE_REDIS"
-sudo sed -i "s/\/\/define('USE_REDIS/\/\/\ndefine('USE_REDIS/" /var/www/html/config.php
+#replaceString "/var/www/config.php" "\/\/define('USE_REDIS" "\/\/\ndefine('USE_REDIS"
+sudo sed -i "s/\/\/define('USE_REDIS/\/\/\ndefine('USE_REDIS/" /var/www/config.php
 
 #todo: logrotate config
 
