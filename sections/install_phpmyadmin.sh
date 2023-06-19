d251dba3b348da54d721a9e9a6beb186b198613a
andy040670 <andy040670@yandex.ru>
2023-06-19 17:32:22 +0300

    Update install_phpmyadmin.sh
    Change home directory ntml to www

--- sections/install_phpmyadmin.sh
+++ sections/install_phpmyadmin.sh
@@ -3,9 +3,9 @@
 showMessage "Installing phpMyAdmin..."
 
 runSudo "wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-english.tar.gz"
-runSudo "tar xzvf phpMyAdmin-5.2.0-english.tar.gz -C /var/www/html/"
+runSudo "tar xzvf phpMyAdmin-5.2.0-english.tar.gz -C /var/www/"
 runSudo "rm phpMyAdmin-5.2.0-english.tar.gz"
-runSudo "mv /var/www/html/phpMyAdmin-5.2.0-english /var/www/html/phpmyadmin"
-runSudo "chown -Rf www-data:www-data /var/www/html"
+runSudo "mv /var/www/html/phpMyAdmin-5.2.0-english /var/www/phpmyadmin"
+runSudo "chown -Rf www-data:www-data /var/www"
 
 showMessage "phpMyAdmin installed."
